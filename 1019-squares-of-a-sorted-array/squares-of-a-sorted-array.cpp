bedf1818e4f0f9469b94bc76c5ece8780df30c56
Naresh Panjiyar <nareshpanjiyar90@gmail.com>
2026-04-05 20:22:41 +0530

    Time: 0 ms (100.00%) | Memory: 30.3 MB (37.28%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -3,44 +3,18 @@ public:
     vector<int> sortedSquares(vector<int>& nums) {
 
         int n = nums.size();
-        vector<int> neg;
-        vector<int> pos;
-       vector<int> ans;
-        for(int i = 0; i < n; i++){
-           
-           if(nums[i] < 0){
-            neg.push_back(pow(nums[i],2));
-           }
-
-           else{
-            pos.push_back(pow(nums[i],2));
-           }
-        }
-        reverse(neg.begin(),neg.end());
-        int i =0,j = 0;
-        int a = neg.size();
-        int b = pos.size();
-
-        while( i < a && j < b){
-
-            if(neg[i] < pos[j]){
-                ans.push_back(neg[i]);
-                i++;
+        vector<int> ans(n);
+        int left = 0, right = n- 1;
+        int k = n - 1;
+        while(left <= right){
+            if(abs(nums[left]) > abs(nums[right])){
+                ans[k--] = nums[left] * nums[left];
+                left++;
             }
-            else{
-                ans.push_back(pos[j]);
-                j++;
+            else {
+                ans[k--] = nums[right] * nums[right];
+                right--;
             }
-        }
-
-        while(i < a) {
-            ans.push_back(neg[i]); 
-            i++;
-
-        }
-        while(j < b) {
-            ans.push_back(pos[j]); 
-            j++;
         }
 
         return ans;
