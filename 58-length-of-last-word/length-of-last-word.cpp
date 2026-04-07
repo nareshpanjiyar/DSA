e0e8e9ce2d055c42041f6f9745a905336bdf4f8b
Naresh Panjiyar <nareshpanjiyar90@gmail.com>
2026-04-07 12:19:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (33.05%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+
+        int i = s.size() - 1;
+
+        // skip spaces
+        while(i >= 0 && s[i] == ' ') i--;
+
+        int len = 0;
+
+        // count last word
+        while(i >= 0 && s[i] != ' '){
+            len++;
+            i--;
+        }
+
+        return len;
+    }
+};
