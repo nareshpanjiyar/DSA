8843cc9c58f8a5be9cd5e0e9557cd218dd7ad0d4
Naresh Panjiyar <nareshpanjiyar90@gmail.com>
2026-04-22 01:14:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (10.23%) - LeetSync

---
+++ 1454-remove-palindromic-subsequences/remove-palindromic-subsequences.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int removePalindromeSub(string s) {
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+
+        if (s == rev) return 1;
+        return 2;
+    }
+};
