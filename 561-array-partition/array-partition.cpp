3ed421290fef515f046dc5d69a4d07560e2513db
Naresh Panjiyar <nareshpanjiyar90@gmail.com>
2026-04-23 22:31:59 +0530

    Time: 8 ms (66.59%) | Memory: 40 MB (20.60%) - LeetSync

--- 561-array-partition/array-partition.cpp
+++ 561-array-partition/array-partition.cpp
@@ -2,12 +2,26 @@ class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
         
-        sort(nums.begin(), nums.end());  // sort array
+        vector<int> freq(20001, 0);  // range -10000 to 10000
+        
+        // store frequency
+        for(int num : nums){
+            freq[num + 10000]++;
+        }
         
         int sum = 0;
+        bool take = true;  // alternate pick
         
-        for(int i = 0; i < nums.size(); i += 2){
-            sum += nums[i];  // take min of each pair
+        for(int i = 0; i < 20001; i++){
+            while(freq[i] > 0){
+                
+                if(take){
+                    sum += (i - 10000);
+                }
+                
+                take = !take;  // flip
+                freq[i]--;
+            }
         }
         
         return sum;
