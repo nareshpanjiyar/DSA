7e0b0e3d8c0e18b3542059aa362277976aafabb8
Naresh Panjiyar <nareshpanjiyar90@gmail.com>
2026-05-06 11:21:25 +0530

    Time: 10 ms (70.76%) | Memory: 56.8 MB (69.53%) - LeetSync

---
+++ 1972-rotating-the-box/rotating-the-box.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
+        
+        int m = box.size();
+        int n = box[0].size();
+
+        // Step 1: Apply gravity
+        for(int i = 0; i < m; i++) {
+            
+            int empty = n - 1;
+
+            for(int j = n - 1; j >= 0; j--) {
+
+                if(box[i][j] == '*') {
+                    empty = j - 1;
+                }
+
+                else if(box[i][j] == '#') {
+
+                    box[i][j] = '.';
+                    box[i][empty] = '#';
+                    empty--;
+                }
+            }
+        }
+
+        // Step 2: Rotate 90 degree clockwise
+        vector<vector<char>> ans(n, vector<char>(m));
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+
+                ans[j][m - 1 - i] = box[i][j];
+            }
+        }
+
+        return ans;
+    }
+};
