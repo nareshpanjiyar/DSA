5f43335a4352180164d8013a5c9dd10aff808788
Naresh Panjiyar <nareshpanjiyar90@gmail.com>
2026-05-12 13:37:36 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (43.35%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -4,14 +4,24 @@ public:
 
         int m = matrix.size();
         int n = matrix[0].size();
+        int low = 0, high = (m * n) - 1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
 
-        for(int i = 0; i< m ;i++){
-            for(int j =0 ; j < n; j++){
-                if(matrix[i][j] == target){
-                    return true;
-                }
+            int row = mid / n;
+            int col = mid % n;
+
+            if(matrix[row][col] == target){
+                return true;
+            }
+            else if(matrix[row][col] < target){
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
             }
         }
+
         return false;
     }
 };
